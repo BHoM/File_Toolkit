d40635a506a9d11de25ded241f2c4ad41d2ec591
Alessio Lombardi <alessio.lombardi@burohappold.com>
2022-01-18 09:37:22 +0000

    Read relies on Serializer_Engine

--- File_Engine/Compute/ReadFromJsonFile.cs
+++ File_Engine/Compute/ReadFromJsonFile.cs
@@ -61,13 +61,7 @@ namespace BH.Engine.Adapters.File
             }
 
             string jsonText = System.IO.File.ReadAllText(fullPath);
-            object converted = null;
-
-            if (!FromJson(jsonText, out converted))
-            {
-                Engine.Base.Compute.RecordWarning($"The content of file `{fullPath}` is not a supported json format.");
-                return new List<object>();
-            }
+            object converted = BH.Engine.Serialiser.Convert.FromJson(jsonText);
 
             // Check if there is any ObjectWrapper that was used to allow writing of non-IObjects, like primitive types (numbers/strings).
             List<object> convertedList = new List<object>();
@@ -87,49 +81,6 @@ namespace BH.Engine.Adapters.File
             }
 
             return new List<object>() { converted };
-        }
-
-        /***************************************************/
-        /**** Private Methods                           ****/
-        /***************************************************/
-
-        private static bool FromJson(string json, out object result)
-        {
-            result = null;
-
-            if (json == "")
-                return false;
-
-            if (json.StartsWith("{"))
-            {
-                BsonDocument document;
-                if (BsonDocument.TryParse(json, out document))
-                {
-                    result = BH.Engine.Serialiser.Convert.FromBson(document);
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-
-            if (json.StartsWith("["))
-            {
-                BsonArray array = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonArray>(json);
-
-                try
-                {
-                    result = array.Select(b => b.IsBsonDocument ? BH.Engine.Serialiser.Convert.FromBson(b.AsBsonDocument) : BH.Engine.Serialiser.Convert.FromJson(b.ToString())).ToList();
-                    return true;
-                }
-                catch
-                {
-                    return false;
-                }
-            }
-
-            return false;
         }
     }
 }
