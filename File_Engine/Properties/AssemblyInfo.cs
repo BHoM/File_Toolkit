9a7eb66b4e6e49c4ec25a55f4196308abd2c9b36
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:05 +0100

    Update Assembly Information

--- File_Engine/Properties/AssemblyInfo.cs
+++ File_Engine/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Filing_Engine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/File_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Filing_Engine")]
